fba1e0cea49eb83b98e95ac7745c0cf1d815462f
kalebsxz <kalebgabriel720@gmail.com>
2025-11-25 20:58:11 -0300

    aula 5

---
+++ YTB/aula5/js/index.js
@@ -0,0 +1,53 @@
+/* VAMOS ENETENDER OPERADORES 
+
+OS OPERADORES JAVASCRIPT SAO USADOS PARA ATRIBUIR, COMPARAR VALORES, EXUCUTAR OPERAÇÃO ARITMÉTICAS E MUITO MAIS.ARITMÉTICAS
+SÃO OS SINAIS QUE UTILIZAMOS: + - * / = ++ -- += -= && || ETC...
+
+
+SÃO SEPARADOS EM 6 CATEGORIAS:
+
+1) OPERADORES ARITMÉTICOS
+2) OPERADORES DE ATRIBUIÇÃO 
+3) OPERADORES DE SEQUENCIA 
+4) OPERADORES DE COMPARAÇÃO 
+5) OPERADOR CONDICIONAL (TERNÁRIO)
+6) OPERADORES LÓGICOS 
+ 
+*/
+
+// OPERADOR ARITMÉTICO 
+
+// var valor1, valor2, total
+
+/* valor1 = 15;
+valor2 = 5;
+total = 0;
+console.log(total = valor1 + valor2);
+console.log(tota1 = valor1 - valor2);
+console.log(tota1 = valor1 / valor2);
+console.log(tota1 = valor1 * valor2); */
+
+// OPERADOR DE ATRIBUIÇÃO 
+
+
+/* var valor1, valor2, total
+
+valor1 += valor2;  */  
+// valor1 + valor2;   Como se fosse um atalho 
+
+
+// OPERADORES DE CONCATENAÇÃO 
+
+/* valor1 = 'Kaleb ';
+valor2 = 'Gabriel';
+
+alert(valor1 + valor2) */
+
+// OPERADORES DE COMPARAÇÃO 
+
+valor1 = 'quinze'
+valor2 = 15
+
+total = (valor1 == valor2);   // vai retornar TRUE ou FALSE 
+
+alert(total)
