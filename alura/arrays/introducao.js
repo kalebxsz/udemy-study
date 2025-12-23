44324a071c360880245749302aadcd97dc3cdd6c
kalebxsz <kalebgabriel720@gmail.com>
2025-12-23 09:59:15 -0300

    introdução array 10%

---
+++ alura/arrays/introducao.js
@@ -0,0 +1,14 @@
+// const nota = 10;
+// const nota1 = 6.5;
+// const nota2 = 8;
+// const nota3 = 7.5;
+const notas = [10, 6.5, 8, 7.5]
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(`A média do aluno foi ${media.toFixed(2)}`);
+
+
+// UM ARRAY É UMA LISTA DE ITENS QUE INICIAM SEMPRE POR 0 E VÃO AO INIFINITO 
+
+let array = ['oi', 16, {a:b}, true, 10]
+console.log(array[0]);
